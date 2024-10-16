15b079180fa2e2c0e833f21eddde409a3c890991
Asmadey1337 <44238872+Asmadey1337@users.noreply.github.com>
2024-10-16 12:41:55 +0300

    Add files via upload

---
+++ WindowsFormsApp1/Form1.cs
@@ -0,0 +1,253 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string path = "Лекція 1.pptx";
+            Process.Start(path);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            string path = "Лекція 2.pptx";
+            Process.Start(path);
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            string path = "Лекція 3.pptx";
+            Process.Start(path);
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            string path = "Лекція 4.pptx";
+            Process.Start(path);
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            string path = "Лекція 5.pptx";
+            Process.Start(path);
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            string path = "Лекція 6.pptx";
+            Process.Start(path);
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            string path = "Лекція 7.pptx";
+            Process.Start(path);
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+            string path = "Лекція 8.pptx";
+            Process.Start(path);
+        }
+
+        private void button9_Click(object sender, EventArgs e)
+        {
+            string path = "Лекція 9.pptx";
+            Process.Start(path);
+        }
+
+        private void button10_Click(object sender, EventArgs e)
+        {
+            string path = "Лекція 10.pptx";
+            Process.Start(path);
+        }
+
+        private void button11_Click(object sender, EventArgs e)
+        {
+            string path = "Лекція 11.pptx";
+            Process.Start(path);
+        }
+
+        private void button12_Click(object sender, EventArgs e)
+        {
+            string path = "Лекція 12.pptx";
+            Process.Start(path);
+        }
+
+        private void button13_Click(object sender, EventArgs e)
+        {
+            string path = "Лекція 13.pptx";
+            Process.Start(path);
+        }
+
+        private void button14_Click(object sender, EventArgs e)
+        {
+            string path = "Лекція 14.pptx";
+            Process.Start(path);
+        }
+
+        private void button15_Click(object sender, EventArgs e)
+        {
+            string path = "Лекція 15.pptx";
+            Process.Start(path);
+        }
+
+        private void button16_Click(object sender, EventArgs e)
+        {
+            string path = "Лекція 16.pptx";
+            Process.Start(path);
+        }
+
+        private void button17_Click(object sender, EventArgs e)
+        {
+            string path = "Лекція 17.pptx";
+            Process.Start(path);
+        }
+
+        private void button18_Click(object sender, EventArgs e)
+        {
+            string path = "ЛР1.docx";
+            Process.Start(path);
+        }
+
+        private void button19_Click(object sender, EventArgs e)
+        {
+            string path = "ЛР2.docx";
+            Process.Start(path);
+        }
+
+        private void button20_Click(object sender, EventArgs e)
+        {
+            string path = "ЛР3.docx";
+            Process.Start(path);
+        }
+
+        private void button21_Click(object sender, EventArgs e)
+        {
+            string path = "ЛР4.docx";
+            Process.Start(path);
+        }
+
+        private void button22_Click(object sender, EventArgs e)
+        {
+            string path = "ЛР5.docx";
+            Process.Start(path);
+        }
+
+        private void button23_Click(object sender, EventArgs e)
+        {
+            string path = "ЛР6.docx";
+            Process.Start(path);
+        }
+
+        private void button24_Click(object sender, EventArgs e)
+        {
+            System.Diagnostics.Process.Start("https://docs.google.com/forms/d/e/1FAIpQLScROjOHGeXFzPw0xh64_zUHfPiXbJmaCXF2DV3aMbJZCc57zw/viewform?usp=sf_link");
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button25_Click(object sender, EventArgs e)
+        {
+            System.Diagnostics.Process.Start("https://docs.google.com/forms/d/e/1FAIpQLSdu5U2qByT7wVxDq6in1KRYCfCtUQ-BMhAxOIomk6nkCOLtLA/viewform?usp=sf_link");
+
+        }
+
+        private void button26_Click(object sender, EventArgs e)
+        {
+            System.Diagnostics.Process.Start("https://docs.google.com/forms/d/e/1FAIpQLSdRfv0sHx4bThrhByMoAhCR1TmawQWNNgyn6Npbj6BNJzYakw/viewform?usp=sf_link");
+        }
+
+        private void button27_Click(object sender, EventArgs e)
+        {
+            System.Diagnostics.Process.Start("https://docs.google.com/forms/d/e/1FAIpQLSfhmKQEi_yOmeMKltr6WXr_i5Jv5pl3GNVHr_BIfmU1eeG1Fg/viewform?usp=sf_link");
+        }
+
+        private void button28_Click(object sender, EventArgs e)
+        {
+            System.Diagnostics.Process.Start("https://docs.google.com/forms/d/e/1FAIpQLScityxIbCUnolrWnNaSE6qz5vuTsCls8dEJH3bxr9PrHgMsoQ/viewform?usp=sf_link");
+        }
+
+        private void button29_Click(object sender, EventArgs e)
+        {
+
+            System.Diagnostics.Process.Start("https://docs.google.com/forms/d/e/1FAIpQLSd5wuDLLD0VPSwyZ4JYkgKGcnNE1afnG0VB4dQCr6C8P_c5uQ/viewform?usp=sf_link");
+        }
+
+        private void button30_Click(object sender, EventArgs e)
+        {
+            System.Diagnostics.Process.Start("https://docs.google.com/forms/d/e/1FAIpQLSeGgzWmgbiOWujPemRAWbW7JNh8pAiz_wJu_VW85iUdnuWrqQ/viewform?usp=sf_link");
+        }
+
+        private void button31_Click(object sender, EventArgs e)
+        {
+            System.Diagnostics.Process.Start("https://docs.google.com/forms/d/e/1FAIpQLScSgA_94-lrnrM1tpNguU3VVFyi_Ys3_oegjYr0ftlUOagKww/viewform?usp=sf_link");
+        }
+
+        private void button32_Click(object sender, EventArgs e)
+        {
+            System.Diagnostics.Process.Start("https://docs.google.com/forms/d/e/1FAIpQLSfwrm829OFBK6OMqQIAqzWnvl0RdI-czcJ1ISIYixHYP1cTCQ/viewform?usp=sf_link ");
+        }
+
+        private void button33_Click(object sender, EventArgs e)
+        {
+            System.Diagnostics.Process.Start("https://docs.google.com/forms/d/e/1FAIpQLSdEEx2htJUbYZSKFEUlf2c3TMo8TdQqQEFiL8u9oMkkJWJxSw/viewform?usp=sf_link ");
+        }
+
+        private void button34_Click(object sender, EventArgs e)
+        {
+            System.Diagnostics.Process.Start("https://docs.google.com/forms/d/e/1FAIpQLScxuEeQL3J15vbyoH8xCrmn7ga1hXlkjwsnDacMpjaMVucUrg/viewform?usp=sf_link");
+        }
+
+        private void button35_Click(object sender, EventArgs e)
+        {
+            System.Diagnostics.Process.Start("https://docs.google.com/forms/d/e/1FAIpQLSeYv8yCuOB2xLfL_axAW17nlywilnZe3S37HgPNjKAyvbURuw/viewform?usp=sf_link");
+        }
+
+        private void button36_Click(object sender, EventArgs e)
+        {
+            System.Diagnostics.Process.Start("https://docs.google.com/forms/d/e/1FAIpQLSfXglM8Kz8BPzJ_2xj1NNMy5IB2OuxM7FmX4E4XSMafQ1fhWQ/viewform?usp=sf_link");
+        }
+
+        private void button38_Click(object sender, EventArgs e)
+        {
+            System.Diagnostics.Process.Start("https://docs.google.com/forms/d/e/1FAIpQLSea44ec4EoRCAikdIpcPkp3mfg5I1e8YURd7qBB_mVtHit7Rg/viewform?usp=sf_link");
+        }
+
+        private void button37_Click(object sender, EventArgs e)
+        {
+            System.Diagnostics.Process.Start("https://docs.google.com/forms/d/e/1FAIpQLSdbVttOqTVrOIEaSPhKPn7JoXLd-8QdZYe8FogD7A93-6IsBg/viewform?usp=sf_link");
+        }
+
+        private void button39_Click(object sender, EventArgs e)
+        {
+            System.Diagnostics.Process.Start("https://docs.google.com/forms/d/e/1FAIpQLSeZEU38oU6qtndQsiQnbH5PSmIZF-1FD217wAzZCDKELQPbEw/viewform?usp=sf_link");
+        }
+
+        private void button40_Click(object sender, EventArgs e)
+        {
+            System.Diagnostics.Process.Start("https://docs.google.com/forms/d/e/1FAIpQLSftzHVkRKvSnj21Q1XqbPYXYE2JGzAHH0Sp2ojkfH-ZCH4N9w/viewform?usp=sf_link");
+        }
+    }
+}
+
